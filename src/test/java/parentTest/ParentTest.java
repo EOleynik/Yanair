4ba273d7fc687a949318cbeb313f634c8351c618
User <Malbox@i.ua>
2019-09-15 23:16:51 +0300

    Parent Test

---
+++ src/test/java/parentTest/ParentTest.java
@@ -0,0 +1,77 @@
+package parentTest;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+public class ParentTest {
+    WebDriver webDriver;
+    String browser = System.getProperty("browser");
+
+    @Before
+    public void setUp() {
+        if (browser == null || "chrome".equals(browser.toLowerCase())) {
+            File file = new File("./src/old_drivers/chromedriver.exe");
+            System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+
+            webDriver = new ChromeDriver();
+        } else if ("firefox".equals(browser.toLowerCase())) {
+            File file = new File("./src/drivers/geckodriver.exe");
+            System.setProperty("webdriver.gecko.driver", file.getAbsolutePath());
+            FirefoxOptions profile = new FirefoxOptions();
+            profile.addPreference("browser.startup.page", 0);
+            profile.addPreference("browser.startup.homepage_overdrive.mstone", "ignore");
+            webDriver = new FirefoxDriver();
+        } else if ("iedriver".equals(browser)) {
+            File file1 = new File("./src/drivers/IEDriverServer.exe");
+            System.setProperty("webdriver.ie.driver", file1.getAbsolutePath());
+            DesiredCapabilities capabilities = DesiredCapabilities.internetExplorer();
+            capabilities.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
+            capabilities.setCapability("ignoreZoomSetting", true);
+            capabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+            webDriver = new InternetExplorerDriver();
+        } else if ("remote".equals(browser)) {
+            try {
+                DesiredCapabilities cap = new DesiredCapabilities();
+                cap.setBrowserName("chrome");
+                cap.setPlatform(Platform.WINDOWS);
+//                cap.setVersion("32");
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.merge(cap);
+                webDriver = new RemoteWebDriver(
+                        new URL("http://localhost:4444/wd/hub"),
+                        chromeOptions);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        webDriver.manage().window().maximize();
+        webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @After
+    public void tearDown() {
+        webDriver.quit();
+    }
+
+    public void checkExpectedResult(String message, boolean expectedResult, boolean actualResult) {
+        Assert.assertEquals(message, expectedResult, actualResult);
+    }
+
+}
